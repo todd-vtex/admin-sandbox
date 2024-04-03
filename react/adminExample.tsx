9b5909078131d433665fd61700148a5cab71eee6
Todd <todd.desantis@vtex.com>
2024-04-03 11:09:22 -0400

    down to a single component for order operator

--- react/adminExample.tsx
+++ react/adminExample.tsx
@@ -4,8 +4,8 @@ import {Layout, PageBlock} from 'vtex.styleguide'
 const AdminExample: FC = () => {
   return (
     <Layout>
-      <PageBlock title="Title"
-                 subtitle="Subtitle here"
+      <PageBlock title="Order Operator Settings"
+                 subtitle="Spam orders to fill out the dashboard"
                  variation="full">
         <h1>Hello, World!</h1>
       </PageBlock>
